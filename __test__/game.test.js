70bfe681386daaa29323e21602e175a3ef03605c
Eric Settels and Jamison Cozart <eric@>
2020-02-05 09:29:59 -0800

    Add test for rooms array inside Game constructor

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,8 +1,12 @@
 import { Game } from './../src/logic.js'
 
 describe('Game', () => {
-  test('should check for successful board constructionk', () => {
+  test('should check for successful Game constructionk', () => {
     const board = new Game();
     expect(typeof board).toEqual("object");
+  })
+  test('should check that Game constructor creates a rooms array of arrays', () => {
+    const board = new Game();
+    expect(board.rooms).toEqual([[],[],[]]);
   })
 })
