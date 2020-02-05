84e15951ca4ffd4f8640a347f6b7726ab0856603
Eric Settels and Jamison Cozart <jamison@>
2020-02-05 10:05:20 -0800

    Refactor jest test to use global variables

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,22 +1,24 @@
 import { Game, Player } from './../src/logic.js'
 
+let game;
+let player;
 
 describe('Game', () => {
+  beforeEach(() => {
+    game = new Game();
+    player = new Player();
+  })
   test('should check for successful Game constructionk', () => {
-    const board = new Game();
-    expect(typeof board).toEqual("object");
+    expect(typeof game).toEqual("object");
   })
   test('should check that Game constructor creates a rooms array of arrays', () => {
-    const board = new Game();
-    expect(board.rooms).toEqual([[],[],[]]);
+    expect(game.rooms).toEqual([[],[],[]]);
   })
   test('should check that Player constructor creates a Player with currentX and currentY values equal to 0', () => {
-    const player = new Player();
     expect(player.currentX).toEqual(0);
     expect(player.currentY).toEqual(0);
   })
   test('should check that the move() method on the player object changes the players current position values correctly', () => {
-    const player = new Player();
     player.move("down");
     expect(player.currentY).toEqual(1);
     player.move("up");
