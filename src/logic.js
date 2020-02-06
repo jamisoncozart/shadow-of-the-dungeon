ee4605874615285e9f10b491ac9128532e6e428f
Eric Settels and Jamison Cozart <eric@>
2020-02-05 16:34:55 -0800

    Add front-end logic and connect it to back-end logic

--- src/logic.js
+++ src/logic.js
@@ -3,11 +3,9 @@ export class Game {
     this.rooms = [[],[],[]];
   }
   addRooms(roomsArray) {
-    for(let i = 0; i < roomsArray.length; i++) {
-      for(let y = 0; y < 3; y++) {
-        for(let x = 0; x < 3; x++) {
-          this.rooms[y][x] = roomsArray[i];
-        }
+    for(let y = 0; y < 3; y++) {
+      for(let x = 0; x < 3; x++) {
+        this.rooms[y][x] = roomsArray[Math.floor(Math.random()*roomsArray.length)];
       }
     }
   }
@@ -19,7 +17,10 @@ export class Player {
     this.currentY = 0;
     this.health = 100;
     this.potions = 0;
-    this.currentWeapon = 'sword';
+    this.currentWeapon = {
+      name: "Exclibur",
+      dmg: 20,
+    };
     }
 
     takeDamage(dmg) {
@@ -43,19 +44,20 @@ export class Player {
         break;
     }
   }
+  enterRoom(Game) {
+    let currentRoom = Game.rooms[this.currentX][this.currentY];
+    return currentRoom.content;
+  }
 }
 
 export class Room {
-  constructor(x, y) {
-    // this.x = x;
-    // this.y = y;
+  constructor() {
     this.content = {};
   }
   buildRoom(type) {
-    this.type = type;
     if(type === 'encounter'){
       let goblin = new Enemy();
-      this.content = goblin   
+      this.content = goblin ;  
     } else if(type === 'loot'){
       let chest = new Loot();
       this.content = chest;
@@ -65,15 +67,35 @@ export class Room {
 
 export class Enemy {
   constructor() {
+    this.dead = false;
     this.name = "Goblin";
     this.health = 50;
-    this.weapon = 'Goblin Sword'
+    this.weapon = {
+      name: 'Goblin Sword',
+      dmg: 15
+    };
+    this.img = 'https://www.stickpng.com/assets/images/5b4eee54c051e602a568ce1b.png';
+  }
+  takeDamage(player){
+    // console.log(player);
+    
+    this.health -= player.currentWeapon.dmg;
+    let message = ''
+    if(this.health <= 0) { 
+      message =` Goblin health: 0, You killed the goblin!` 
+      this.dead = true
+    }
+    else if(this.health > 0 ) { message= ` Goblin health: ${this.health}, The goblin took ${player.currentWeapon.dmg} damage`}
+    // console.log( player.takeDamage(this.weapon.dmg));
+    return message
   }
 }
 
 export class Loot {
   constructor(){
+    this.name = "Loot!";
     this.potions = 1;
     this.gold = 10;
+    this.img = 'https://gamepedia.cursecdn.com/paladins_gamepedia/thumb/2/28/Colossal_Chest.png/300px-Colossal_Chest.png?version=7e43e9ee467cc2fa7a912c6c16638c95';
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { Game, Player, Room } from './logic.js';
+import $ from 'jquery';
+import './styles.css'
 
 const game = new Game();
 const roomTypes = ['encounter', 'loot'];
@@ -12,8 +14,58 @@ const room7 = new Room();
 const room8 = new Room();
 const room9 = new Room();
 const roomsArray = [room1, room2, room3, room4, room5, room6, room7, room8, room9];
-roomsArray.forEach( room => {
-  var temp = Math.floor((Math.random()*2)-1)
-  room.buildRoom(roomTypes[temp])
+
+
+const displayOptions = (name)=> {
+  if(name === 'Goblin') {
+    $("#button-cont").hide();
+    $("#goblinBtns").show();
+  }
+
+}
+
+$(document).ready(function() {
+  for(let i = 0; i < roomsArray.length; i++) {
+    let temp = Math.round(Math.random());
+    roomsArray[i].buildRoom(roomTypes[temp]);
+  }
+  game.addRooms(roomsArray);
+  const player = new Player();
+  // let currentRoom= {};
+  $("#output").html(`<img src='${player.enterRoom(game)}' alt='something'>`);
+  //click listeners for movement buttons
+  $("#button-cont").on('click', 'button', event => {
+    player.move(event.target.id)
+    $('#output').empty()
+   let currentRoom = player.enterRoom(game);
+    $("#output").html(`<div class="room"><img src='${currentRoom.img}' alt='something'><h2 class="title">${currentRoom.name}</h2><hr><p class="description"></p></div>`)
+    displayOptions(currentRoom.name);
+    // console.log(game);
+    // console.log(player);
+    console.log(currentRoom.dead);
+    if(currentRoom.dead){
+      $('#message').text("You have allready killed this goblin")
+      $("#button-cont").show();
+      $("#goblinBtns").hide();
+    }else {
+    $("#goblinBtns").on('click', 'button', event => {
+    
+        if(event.target.id === "fight") {
+          $('#message').text(currentRoom.takeDamage(player))
+          if(currentRoom.health < 0 ){
+            $("#button-cont").show();
+            $("#goblinBtns").hide();
+          }
+      
+      } else if(event.target.id === "run") {
+        console.log("You coward");
+      }
+      
+      
+    })
+  }
+
+  })
+  //click listeners for goblin buttons
+ 
 })
-console.log(roomsArray);
